cacaa66a55bb94a46e3ac5c9220042fca315a341
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-11-29 12:47:08 +0900

    Add files via upload

--- js/banner01.js
+++ js/banner01.js
@@ -7,11 +7,19 @@ $(function () {
         $('.topBanner2 .inner').slideToggle(500);
     });
 
+    $('.leftBanner i').on('click', function () {
+        $('.leftBanner').toggleClass('on');
+    });
+
+    $('.rightBanner i').on('click', function () {
+        $('.rightBanner').toggleClass('on');
+    });
+
     $('.bottomBanner i').on('click', function () {
         $('.bottomBanner .inner').slideToggle();
     });
 
     $('.popup button').on('click', function () {
         $(this).parent().hide();
-    })
+    });
 })
--- js/graff.js
+++ js/graff.js
@@ -1,6 +1,17 @@
 $(function () {
+    // topBanner
     $('.topBanner i').on('click', function () {
         $('.topBanner').addClass('on')
-    })
+    });
+
+
+    // mainSlide
+    $('.mainSlide').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
 
 })
