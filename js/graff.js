c639658b7927754eac1306841023e03eaec98764
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-12-01 09:06:56 +0900

    Add files via upload

--- js/graff.js
+++ js/graff.js
@@ -14,4 +14,27 @@ $(function () {
         pauseOnFocus: false,
     });
 
+    $('.mainSlide').on('afterChange', function (e, s, c) {
+        $('.main_dots li').eq(c).addClass('on').siblings().removeClass('on');
+        $('.mainVisual .main_num').text(c + 1);
+    });
+
+    $('.main_dots li').on('click', function () {
+        var idx = $(this).index();
+        $('.mainSlide').slick('slickGoTo', idx);
+    });
+
+    $('.main_pro_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 5,
+    });
+
+    $('.mainProduct .arrows .prev').on('click', function () {
+        $('.main_pro_slide').slick('slickPrev');
+    });
+    $('.mainProduct .arrows .next').on('click', function () {
+        $('.main_pro_slide').slick('slickNext');
+    });
+
 })
