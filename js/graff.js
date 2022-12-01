813af5d09373da15e74ae19582278ff9332ae747
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-12-01 15:37:37 +0900

    Add files via upload

--- js/graff.js
+++ js/graff.js
@@ -37,4 +37,27 @@ $(function () {
         $('.main_pro_slide').slick('slickNext');
     });
 
+    // tab menu
+
+    $('.mainCustomer .tab_menu>li').on('click', function (event) {
+        event.preventDefault();
+        var _this = $(this);  //이벤트가 적용된 자신을
+        var idx = $(this).index();  //이벤트가 적용된 자신의 번호를 반환
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.mainCustomer .tab_content>li').eq(idx).addClass('on').siblings().removeClass('on');
+    })
+
+
+    // 새창으로 연결하는 명령
+    $('#f_link').on('change', function () {
+        var link = $(this).val();
+
+        if (link) {
+            window.open(link)
+        }
+        // link && window.open(link) 간단하게적는거
+    })
+
+
 })
+
